3b22641c59abde4b01eb55dd726c4842734c8ace
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-07 17:38:52 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- m8/p1/assignment1.py
+++ m8/p1/assignment1.py
@@ -1,21 +1,17 @@
-# Exercise: Assignment-1
+'''Exercise: Assignment-1'''
 # This function takes in one number and returns one number.
-def fact_rec(n):
+def fact_rec(n_v):
     '''
     n is positive Integer
     returns: a positive integer, the factorial of n
     '''
     # Your code here
-    if n <= 1:
+    if n_v <= 1:
         return 1
-    else:
-        return n*fact_rec(n-1)
-    
-
-
+    return n_v*fact_rec(n_v-1)
 def main():
-    a = input()
-    print(fact_rec(int(a)))    
-
+    """def fun"""
+    a_v = input()
+    print(fact_rec(int(a_v)))
 if __name__ == "__main__":
     main()
