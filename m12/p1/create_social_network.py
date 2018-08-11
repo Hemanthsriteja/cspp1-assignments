33dbe7ea4542b07be7ea56694df49e75bee62009
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-11 12:32:50 +0530

    " -> 0 of 3 passed. pylint: 8.89/10 "

--- m12/p1/create_social_network.py
+++ m12/p1/create_social_network.py
@@ -38,7 +38,7 @@ def create_social_network(data):
     for i in data:
         print(i)
         y_v = i.split("follows")
-        d_v[y_v[-1]] = y_v[-1].split(",")
+        d_v[y_v[0]] = y_v[-1].split(",")
     return d_v
 def main():
     '''
