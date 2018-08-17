d1c56801aae3ec30568f1eb50f97b85a88b1e480
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-17 11:27:00 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- m11/p3/assignment3.py
+++ m11/p3/assignment3.py
@@ -1,13 +1,23 @@
 # Assignment-3
 '''
-At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
+At this point, we have written code to generate a random hand and display
+that hand to the user. We can also ask the user for a word (Python's input)
+and score the word (using your getWordScore). However, at this point we have
+not written any code to verify that a word given by a player obeys the rules
+of the game. A valid word is in the word list; and it is composed entirely
+of letters from the current hand. Implement the isValidWord function.
 
-Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
+Testing: Make sure the test_isValidWord tests pass. In addition, you
+will want to test your implementation by calling it multiple times on
+the same hand - what should the correct behavior be? Additionally, the
+empty string ('') is not a valid word - if you code this function correctly,
+you shouldn't need an additional check for this condition.
 
-Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
+Fill in the code for isValidWord in ps4a.py and be sure you've passed
+the appropriate tests in test_ps4a.py before pasting your function definition here.
 '''
 
-def isValidWord(word_v, hand_v, wordlist_v):
+def is_valid_word(word_v, hand_v, wordlist_v):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
@@ -17,23 +27,24 @@ def isValidWord(word_v, hand_v, wordlist_v):
     wordList: list of lowercase strings
     """
     c_v = 0
-    l = len(wordlist_v)
+    l_v = len(wordlist_v)
     if word_v in wordlist_v:
         for i in hand_v:
             if i in word_v:
                 c_v = c_v+1
-    if c_v == l:
+    if c_v == l_v:
         return True
     return False
 def main():
+    '''func'''
     word_v = input()
     n_v = int(input())
     adict = {}
-    for i in range(n_v):
+    for _ in range(n_v):
         data = input()
-        l = data.split()
-        adict[l[0]] = int(l[1])
-    l2 = list(input().split())
-    print(isValidWord(word_v,adict,l2))
+        l_v = data.split()
+        adict[l_v[0]] = int(l_v[1])
+    l2_v = list(input().split())
+    print(is_valid_word(word_v, adict, l2_v))
 if __name__ == "__main__":
     main()
