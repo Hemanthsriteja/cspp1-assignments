d113b0deaefeb77b00a95b3e8997bd84bcf5038d
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-18 17:28:30 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- m14/CodeCampPoker/poker.py
+++ m14/CodeCampPoker/poker.py
@@ -1,76 +1,93 @@
-'''
-    Write a program to evaluate poker hands and determine the winner
-    Read about poker hands here.
-    https://en.wikipedia.org/wiki/List_of_poker_hands
-'''
+"""# import collections"""
+
+def card_value_hand(hand):
+    '''calculates card values'''
+    exception_case = [2, 3, 4, 5, 14]
+    card_value = ['--23456789TJQKA'.index(c) for c, s in hand]
+    if card_value == exception_case:
+        card_value.remove(14)
+        card_value.add(1)
+    card_value = sorted(card_value, reverse=True)
+    return card_value
 
 def is_straight(hand):
-    '''
-        How do we find out if the given hand is a straight?
-        The hand has a list of cards represented as strings.
-        There are multiple ways of checking if the hand is a straight.
-        Do we need both the characters in the string? No.
-        The first character is good enough to determine a straight
-        Think of an algorithm: given the card face value how to check if it a straight
-        Write the code for it and return True if it is a straight else return False
-    '''
-    pass
+    ''' Straight function '''
+    return len(card_value_hand(hand)) == 5 and (max(card_value_hand(
+        hand))-min(card_value_hand(hand)) == 4)
 
 def is_flush(hand):
-    '''
-        How do we find out if the given hand is a flush?
-        The hand has a list of cards represented as strings.
-        Do we need both the characters in the string? No.
-        The second character is good enough to determine a flush
-        Think of an algorithm: given the card suite how to check if it is a flush
-        Write the code for it and return True if it is a flush else return False
-    '''
-    pass
+    ''' flush function '''
+    return len(set(s for c, s in hand)) == 1
+
+def kind(hand, n_len):
+    ''' To determine the kind of the function'''
+    for ranks in card_value_hand(hand):
+        if card_value_hand(hand).count(ranks) == n_len:
+            return ranks
+    return None
+
+# def is_four_of_a_kind(hand):
+#     ''' Four of a kind hand function '''
+#     return list(collections.Counter(card_value_hand(hand)).values()) == [4, 1]
+
+# def is_fullhouse(hand):
+#     ''' Full House hand function '''
+#     return sorted(list(collections.Counter(card_value_hand(hand)).values())) == [2, 3]
+
+# def is_three_of_kind(hand):
+#     ''' Three of kind hand function '''
+#     bool_val = sorted(list(collections.Counter(card_value_hand(hand)).values())) == [1, 1, 3]
+#     return bool_val
+
+# def is_twopair(hand):
+#     ''' Two pair hand function '''
+#     bool_val = sorted(list(collections.Counter(card_value_hand(hand)).values())) == [1, 2, 2]
+#     return bool_val
+
+# def is_onepair(hand):
+#     ''' One pair hand function '''
+#     bool_val = sorted(list(collections.Counter(card_value_hand(hand)).values())) == [1, 1, 1, 2]
+#     return bool_val
 
 def hand_rank(hand):
-    '''
-        You will code this function. The goal of the function is to
-        return a value that max can use to identify the best hand.
-        As this function is complex we will progressively develop it.
-        The first version should identify if the given hand is a straight
-        or a flush or a straight flush.
-    '''
+    ''' ranks the card '''
+    hand_ranks = card_value_hand(hand)
+    rank = (0, hand_ranks)
+    if is_flush(hand) and is_straight(hand):
+        rank = (8,)
 
-    # By now you should have seen the way a card is represented.
-    # If you haven't then go the main or poker function and print the hands
-    # Each card is coded as a 2 character string. Example Kind of Hearts is KH
-    # First character for face value 2,3,4,5,6,7,8,9,T,J,Q,K,A
-    # Second character for the suit S (Spade), H (Heart), D (Diamond), C (Clubs)
-    # What would be the logic to determine if a hand is a straight or flush?
-    # Let's not think about the logic in the hand_rank function
-    # Instead break it down into two sub functions is_straight and is_flush
+    elif kind(hand, 4):
+        rank = (7,)
 
-    # check for straight, flush and straight flush
-    # best hand of these 3 would be a straight flush with the return value 3
-    # the second best would be a flush with the return value 2
-    # third would be a straight with the return value 1
-    # any other hand would be the fourth best with the return value 0
-    # max in poker function uses these return values to select the best hand
-    return 1
+    elif kind(hand, 3) and kind(hand, 2):
+        rank = (6, )
+
+    elif is_flush(hand):
+        rank = (5, hand_ranks)
+
+    elif is_straight(hand):
+        rank = (4, hand_ranks)
+
+    elif kind(hand, 3):
+        return (3, kind(hand, 2), hand_ranks)
+
+    elif kind(hand, 2) and kind(sorted(hand, reverse=True), 2) and kind(\
+    hand, 2) != kind(sorted(hand, reverse=True), 2):
+        return (2, kind(hand, 2), kind(sorted(hand, reverse=True), 2))
+
+    elif kind(hand, 2):
+        return (1, kind(hand, 2))
+    return rank
 
 def poker(hands):
     '''
         This function is completed for you. Read it to learn the code.
 
         Input: List of 2 or more poker hands
-               Each poker hand is represented as a list
                Print the hands to see the hand representation
 
         Output: Return the winning poker hand
     '''
-
-    # the line below may be new to you
-    # max function is provided by python library
-    # learn how it works, in particular the key argument, from the link
-    # https://www.programiz.com/python-programming/methods/built-in/max
-    # hand_rank is a function passed to max
-    # hand_rank takes a hand and returns its rank
-    # max uses the rank returned by hand_rank and returns the best hand
     return max(hands, key=hand_rank)
 
 if __name__ == "__main__":
