65b49019255e03448f932a8e83cb2e34dd2320dd
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 10:52:17 +0530

    " -> 0 of 3 passed. pylint: 5.45/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -9,7 +9,7 @@ def clean_string(string):
     # regex = re.compile('[^A-Z ]')
     # regex = re.compile('[^0-9 ]')
 
-    string = regex.sub(, string)
+   # string = regex.sub(, string)
     list_ofwords = string.split(' ')
     for index in range(len(list_ofwords)):
         list_ofwords[index] = list_ofwords[index].strip()
