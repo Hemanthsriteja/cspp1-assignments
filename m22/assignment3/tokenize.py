4fa764dd10d4e130c38b1219ed4e6ef031c49d61
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 12:11:01 +0530

    " -> 0 of 4 passed. pylint: 0.00/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -5,13 +5,12 @@ each word
 
 def tokenize(string):
 	freq = {}
-    for i_i in string:
-        freq[i_i] = freq.get(i_i, 0) + 1
-    return freq
-    
+	for i_i in string:
+		freq[i_i] = freq.get(i_i, 0) + 1
+	return freq
 
 def main():
-    string = input()
+	string = input()
 
 if __name__ == '__main__':
-    main()
+	main()
