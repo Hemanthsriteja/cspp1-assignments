c5ba170d10b3d4b7567f4724789ed4a40f35a688
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 11:26:19 +0530

    " -> 2 of 3 passed. pylint: 10.00/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -7,8 +7,7 @@ import re
 def clean_string(string):
     """clean sring"""
     regex = re.compile(r'[^a-z ]')
-    rgx1 = regex.compile(r'[0,1,2,3,4,5,6,7,8,9]')
-    string = rgx1.sub('', string)
+    string = regex.sub('', string)
     str1 = string.replace(" ", "")
     return str1
 def main():
