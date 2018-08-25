7f85eafcad1c522922e5849895f8273339fce8e9
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 12:27:17 +0530

    " -> 0 of 4 passed. pylint: 6.92/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -3,14 +3,34 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 
-def tokenize(string):
+def tokenize(string1):
     freq = {}
-    for i_i in string:
+    for i_i in string1:
         freq[i_i] = freq.get(i_i, 0) + 1
     return freq
             
 def main():
     string = input()
+    for _ in range(int(string)):
+        data = input()
+        string1 = data.split()
+    tokenize(string1)
+
+#     a_v = {}
+#     for j in word_v:
+#         if j in hand_v:
+#             a_v[j] = hand_v[j]-1
+#     return a_v
+# def main():
+#     '''main'''
+#     n_v = input()
+#     a_dict = {}
+#     for _ in range(int(n_v)):
+#         data = input()
+#         l_v = data.split()
+#         a_dict[l_v[0]] = int(l_v[1])
+#     data1_v = input()
+#     print(update_hand(a_dict, data1_v))
     
 
 if __name__ == '__main__':
