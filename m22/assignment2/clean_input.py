d2dcfc4b8ad682b92e7743672b0a4bfd4471a888
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 11:02:05 +0530

    " -> 1 of 3 passed. pylint: 7.27/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -6,7 +6,7 @@ import re
 
 def clean_string(string):
     regex = re.compile('[^a-z ]')
-    #regex = re.compile('[^0-9 ]')
+    regex = re.compile('[^0,1,2,3,4,5,6,7,8,9]')
 
     string = regex.sub('', string)
     return string
