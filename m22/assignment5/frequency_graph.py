765bcfbbbf2d2c29327da92efc99acc8d4ae6cff
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 12:03:35 +0530

    " -> 4 of 4 passed. pylint: 8.89/10 "

--- m22/assignment5/frequency_graph.py
+++ m22/assignment5/frequency_graph.py
@@ -4,11 +4,13 @@ frequency of each word. Display the frequency values using “#” as a text bas
 '''
 
 def frequency_graph(dict_v):
+    """freq graph"""
     keys = sorted(dict_v.keys())
     for key in keys:
         print(key, "-", dict_v[key]*'#')
 
 def main():
+    """main func"""
     dict_v = eval(input())
     frequency_graph(dict_v)
 
