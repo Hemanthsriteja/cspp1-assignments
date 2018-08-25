7dc1e13e7bbce49145bb8cf28903fdc2e3c5d7c6
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 11:04:58 +0530

    " -> 1 of 3 passed. pylint: 5.71/10 "

--- m22/assignment2/clean_input.py
+++ m22/assignment2/clean_input.py
@@ -8,13 +8,13 @@ def clean_string(string):
     regex = re.compile('[^a-z ]')
     
 
-    string = regex.sub(' ', string)
+    string = regex.sub('', string)
     return string
-    # list_ofwords = string.split(' ')
-    # for index in range(len(list_ofwords)):
-    #     list_ofwords[index] = list_ofwords[index].strip()
-    # # print(list_ofwords)
-    # return list_ofwords
+    list_ofwords = string.split(' ')
+    for index in range(len(list_ofwords)):
+        list_ofwords[index] = list_ofwords[index].strip()
+    # print(list_ofwords)
+    return list_ofwords
 
 def main():
     string = input()
