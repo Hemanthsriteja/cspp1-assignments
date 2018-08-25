5bbea7c2c754403335e36290b4f579e3e97c7811
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-25 11:18:24 +0530

    " -> 2 of 2 passed. pylint: 9.23/10 "

--- m22/assignment1/read_input.py
+++ m22/assignment1/read_input.py
@@ -1,24 +1,9 @@
 '''
 Write a python program to read multiple lines of text input and store the input into a string.
 '''
-
 def mult_line(input_v):
-
     list_1 = input_v
-        #list_1 = list_1.split("\n")
     return list_1
-    # i = 2
-    # for set_list in list_1:
-    #     if " follows " in set_list:
-    #         list_i = set_list
-    #         i += 1
-    #         if list_i != '':
-    #             list_i = list_i.split(' follows ')
-    #             #print(list_i)
-    #             NETDICT[list_i[0]] = list_i[1].split(',')
-    # return NETDICT
-
-
 def main():
     '''
         handling testcase input and printing output
